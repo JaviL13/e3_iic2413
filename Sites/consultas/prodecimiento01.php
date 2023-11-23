dea490f82335f42f9d89b2c11701fd9467560cfb
Javiera Larraín <javipaz.larrain@uc.cl>
2023-11-23 17:08:49 -0300

    cambios

--- Sites/consultas/prodecimiento01.php
+++ Sites/consultas/prodecimiento01.php
@@ -8,10 +8,42 @@
     $nombre_tabla = $_GET['nombre_tabla'];
     $criterio = $_GET['criterio'];
 
-    echo "$atributos  $nombre_tabla $criterio ";
+    
+    $query = "SELECT $atributos FROM $nombre_tabla WHERE $criterio";
+    $result = $db -> prepare($query);
+    $result -> execute();
+    $dataCollected = $result->fetchAll(PDO::FETCH_ASSOC);
+    $columnHeaders = array_keys($dataCollected[0]);
 
+    echo "$atributos  $nombre_tabla $criterio ";
     ?>
 
-    
+    <div class='container' style="width: 70%">
+        <h3>Resultado de la consulta:</h3>
+        <table class="table">
+            <thead>
+                <tr>
+                    <?php
+                    // Imprimir encabezados de columnas
+                    foreach ($columnHeaders as $column) {
+                        echo "<th>$column</th>";
+                    }
+                    ?>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                // Imprimir filas de la tabla
+                foreach ($dataCollected as $row) {
+                    echo "<tr>";
+                    foreach ($row as $value) {
+                        echo "<td>$value</td>";
+                    }
+                    echo "</tr>";
+                }
+                ?>
+            </tbody>
+        </table>
+    </div>
 
 <?php include('../templates/footer.html'); ?>
